a62a7370ba6fb74fd6eb1a630758e7c4bf527a9a
IvanTron <99278095+IvanRoger20@users.noreply.github.com>
2022-03-14 22:33:07 -0700

    Update EXAMEN_1RA_EVALUACION_KITKAT.java

--- src/examen_1ra_evaluacion_kitkat/EXAMEN_1RA_EVALUACION_KITKAT.java
+++ src/examen_1ra_evaluacion_kitkat/EXAMEN_1RA_EVALUACION_KITKAT.java
@@ -1,8 +1,6 @@
 package examen_1ra_evaluacion_kitkat;
 
-import clases_examen.CLASE_EMPLEADO;
 import clases_examen.DatosIntegrantes;
-import clases_examen.Password;
 
 public class EXAMEN_1RA_EVALUACION_KITKAT {
 
@@ -11,25 +9,7 @@ public class EXAMEN_1RA_EVALUACION_KITKAT {
         System.out.println("------------------------------------------");
         DatosIntegrantes.imprimirDatosEquipo();
         DatosIntegrantes.imprimirProblemasResueltos();
-        System.out.println("------------------------------------------");
-        System.out.println("Problema clase contraseña");
-        Password MiContra = new Password();
-        MiContra.setUsuario("xXx_PepeMax_xXx");
-        MiContra.setContraseña("itachi23");
-        if (MiContra.verificarAcceso(MiContra.getUsuario(), MiContra.getContraseña()) == true) {
-            System.out.println("El usuario y contraseña es valido");
-        } else {
-            System.out.println("El usuario o contraseña no es valido");
-        }
-        System.out.println("------------------------------------------");
-        System.out.println("Problema Clase empleado");
-        CLASE_EMPLEADO empleado = new CLASE_EMPLEADO();
-        empleado.setNombre("Juan Manuel");
-        empleado.setApellido("Rosa Mendoza");
-        empleado.setDireccion("Calle Robinson #30132");
-        empleado.setSalario(45231);
-        empleado.setAñoIngreso(2010);
-        empleado.imprimirDatos();
+        System.out.println("Equipo KitKat");
         System.out.println("------------------------------------------");
     }
 
